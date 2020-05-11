aa77e9cb0bab072819dd99a6522e6a68d9a43917
erikbrunno <erik.brunno@yahoo.com.br>
2020-05-11 12:14:30 -0300

    animacoes na tela inicial

--- instagran/src/app/acesso/banner/banner.component.ts
+++ instagran/src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Imagem } from './imagem.model'
 
 @Component({
   selector: 'app-banner',
@@ -12,7 +13,9 @@ import { trigger, state, style } from '@angular/animations';
       })),
       state('visivel', style({
         opacity: 1
-      }))
+      })),
+      transition('escondido <=> visivel', animate('2s ease-in')),
+
     ])
   ]
 })
@@ -20,9 +23,40 @@ export class BannerComponent implements OnInit {
 
   public estado: string = 'escondido'
 
+  //Auxilia na exibicao da imagem seguinte
+  public idx: number
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url:'../assets/banner-acesso/img_1.png'},
+    { estado: 'escondido', url:'../assets/banner-acesso/img_2.png'},
+    { estado: 'escondido', url:'../assets/banner-acesso/img_3.png'},
+    { estado: 'escondido', url:'../assets/banner-acesso/img_4.png'},
+    { estado: 'escondido', url:'../assets/banner-acesso/img_5.png'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
+  
+  logicaRotacao(): void {
+    for(let i:number = 0; i <= 4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido'
+
+        this.idx = i === 4 ? 0 : i + 1
+        break
+      }
+    }
+
+    this.imagens[this.idx].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000)
+  }
+
+  toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel'
+  }
 }
